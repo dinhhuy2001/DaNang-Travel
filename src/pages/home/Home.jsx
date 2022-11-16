bf791f835df0643c46fabd537593312fae8c08f2
linh2001ht <39437185+linh2001ht@users.noreply.github.com>
2022-11-16 16:56:41 +0000

    init commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from '../../components/Footer';
 import Hero from '../../components/Hero';
+import Ads from '../../components/Ads';
 import Navbar from '../../components/Navbar';
 import Recommend from '../../components/Recommend';
 import ScrollToTop from '../../components/ScrollToTop';
@@ -14,6 +15,7 @@ const Home = () => {
             <ScrollToTop />
             <Navbar />
             <Hero />
+            <Ads />
             <Top />
             <Recommend />
             <Subcribe />
